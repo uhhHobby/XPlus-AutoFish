f9412b631308922caa7cb29187fc0d045bcf75ca
Wudji <1215168278@qq.com>
2024-06-15 20:12:31 +0800

    Cleanup code

--- src/main/java/troy/autofish/Autofish.java
+++ src/main/java/troy/autofish/Autofish.java
@@ -18,8 +18,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.StringHelper;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import troy.autofish.monitor.FishMonitorMP;
 import troy.autofish.monitor.FishMonitorMPMotion;
 import troy.autofish.monitor.FishMonitorMPSound;
