1040f5cfa4f0111f1cf36e8a85a15e3d0b662d37
Wudji <1215168278@qq.com>
2024-08-14 13:26:07 +0800

    Add water check for persistentMode

--- src/main/java/troy/autofish/Autofish.java
+++ src/main/java/troy/autofish/Autofish.java
@@ -47,7 +47,7 @@ public class Autofish {
         modAutofish.getScheduler().scheduleRepeatingAction(10000, () -> {
             if(!modAutofish.getConfig().isPersistentMode()) return;
             if(!isHoldingFishingRod()) return;
-            if(hookExists) return;
+            if(hookExists && isBobberInWater()) return;
             if(modAutofish.getScheduler().isRecastQueued()) return;
 
             useRod();
@@ -229,6 +229,14 @@ public class Autofish {
                     }
                 }
             }
+        }
+    }
+
+    public boolean isBobberInWater(){
+        if(client.player != null && client.world != null && client.player.fishHook != null) {
+            return client.player.fishHook.isTouchingWater();
+        } else{
+            return false;
         }
     }
 
